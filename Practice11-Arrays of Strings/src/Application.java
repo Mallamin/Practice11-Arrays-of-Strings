b99c44beb217c9a840559cd3a54be3012c86f69a
mohamed.jalloh <mohamed.jalloh@WEPLUS97.wenet.local>
2022-02-15 16:54:10 +0100

    Arrays of Strings

---
+++ Practice11-Arrays of Strings/src/Application.java
@@ -0,0 +1,20 @@
+
+public class Application {
+	public static void main(String[] args) {
+		String [] words=new String[3];
+		words[0]="My";
+		words[1]="Name";
+		words [2]="Jalloh";
+		
+		System.out.println(words[0]);
+		
+		
+		String []fruits={"Mango","Banana", "Guava"};
+		for (String fruit:fruits) {
+			
+			System.out.println(fruit);
+			
+		}
+	}
+
+}
